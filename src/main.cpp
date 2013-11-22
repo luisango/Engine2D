0f60c0f927ceea755c5e244c1404d223c5d9bdc4
Luis Lalueza <luis.lalueza@gmail.com>
2013-11-22 18:02:34 +0100

    P3 scaled and angle

--- src/main.cpp
+++ src/main.cpp
@@ -22,20 +22,39 @@ int main(int argc, char* argv[])
     // Tarea: Cargar la imagen "data/ball.png"
     Image * img = ResourceManager::Instance().LoadImage("data/ball.png");
 
+	double angle = 0;
+	double scale = 1;
+	double scale_step = 0.5;
+	double scale_state = 1;
+
     // Tarea: centrar la imagen
     img->SetMidHandle();
-
+	
     while(Screen::Instance().IsOpened() && !Screen::Instance().KeyPressed(GLFW_KEY_ESC))
     { 
         // TAREA: Actualizar ángulo y escala de la imagen
-
+		if (scale_state) {
+			if (scale + scale_step < 5)
+				scale += scale_step * Screen::Instance().ElapsedTime();
+			else
+				scale_state = 0;
+		} else {
+			if (scale - scale_step > 1)
+				scale -= scale_step * Screen::Instance().ElapsedTime();
+			else
+				scale_state = 1;
+		}
+		
+		Screen::Instance().SetTitle("SCALE = " + String::FromFloat(img->GetWidth()*scale));
         // TAREA: Limpiar pantalla y dibujar la imagen
         Renderer::Instance().Clear();
-
-        Renderer::Instance().DrawImage(img, 200, 200, 0, 300, 300, 0);
+		
+		Renderer::Instance().DrawImage(img, WIDTH/2, HEIGHT/2, 0, img->GetWidth()*scale, img->GetHeight()*scale, WrapValue(angle, 360));
 
         // Refrescamos la pantalla 
         Screen::Instance().Refresh(); 
+
+		angle += 20 * Screen::Instance().ElapsedTime();
     } 
 
     ResourceManager::Instance().FreeResources();
