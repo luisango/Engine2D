fc6bd911c94ea7c60be802502ad8c1b34d8873a7
unknown <luis.lalueza@WKSALMPU1152.u-tad.es>
2013-12-04 18:27:48 +0100

    2d javier, pausa

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,121 @@
+
 #pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
+
+#include "include/u-gine.h"
+#include <math.h>
+#include <time.h>
+
+const int WIDTH = 800;
+const int HEIGHT = 600;
+const bool FULLSCREEN = false;
+
+void randomizeColor(Sprite * spr)
+{
+    spr->SetColor(
+        Random(0, 255), // r
+        Random(0, 255), // g
+        Random(0, 255), // b
+        Random(0, 255)  // a
+    );
+}
+
+int main(int argc, char* argv[])
+{ 
+    Screen& screen            = Screen::Instance();
+    const Renderer& renderer  = Renderer::Instance();
+    ResourceManager& rm       = ResourceManager::Instance(); 
+
+	srand(time(NULL));
+
+    screen.Open(WIDTH, HEIGHT, FULLSCREEN); 
+
+    // Load speed
+    //             v------ (-1) porque el punto inicial colisiona con 0 y cambia la velocidad
+    int speed_x = -1 * Random(128, 255);
+    int speed_y = -1 * Random(128, 255);
+
+    // Load font & create sprite
+    Font   * font_mono   = rm.LoadFont("data/arial16.png");
+    Sprite * sprite_mono = new Sprite(font_mono);
+
+    // Set blend mode by default
+    sprite_mono->SetBlendMode(Renderer::BlendMode::ALPHA);
+
+    // Set initial color
+    randomizeColor(sprite_mono);
+
+    // Set string
+	String text = "Hello, world!";
+
+    // Get width and heigth
+    double text_width  = font_mono->GetTextWidth(text);
+    double text_height = font_mono->GetTextHeight(text);
+    
+    // Set initial position
+	sprite_mono->SetX(0);
+	sprite_mono->SetY(0);
+
+	while(screen.IsOpened() && !screen.KeyPressed(GLFW_KEY_ESC))
+	{
+		// Clear
+		renderer.Clear(0, 0, 0);
+		
+        sprite_mono->MoveTo(
+			sprite_mono->GetX() + speed_x, 
+			sprite_mono->GetY() + speed_y, 
+			speed_x, 
+			speed_y
+		);
+		
+        // Update
+        sprite_mono->Update(screen.ElapsedTime());
+
+		// Colisiones
+        if ((int) sprite_mono->GetX() < 0) {
+			speed_x *= -1;
+            sprite_mono->SetX(0);
+            randomizeColor(sprite_mono);
+        }
+
+        if ((int) sprite_mono->GetX() + text_width > WIDTH) {
+            speed_x *= -1;
+            sprite_mono->SetX(WIDTH - text_width);
+            randomizeColor(sprite_mono);
+        }
+        
+        if ((int) sprite_mono->GetY() < 0) {
+            speed_y *= -1;
+            sprite_mono->SetY(0);
+            randomizeColor(sprite_mono);
+        }
+
+        if ((int) sprite_mono->GetY() + text_height > HEIGHT) {
+            speed_y *= -1;
+            sprite_mono->SetY(HEIGHT - text_height);
+            randomizeColor(sprite_mono);
+        }
+
+		screen.SetTitle("SPEED X = " + String::FromFloat(speed_x) + " SPEED Y = "+ String::FromFloat(speed_y));
+
+		renderer.SetColor(
+			sprite_mono->GetRed(),
+			sprite_mono->GetGreen(),
+			sprite_mono->GetBlue(),
+			sprite_mono->GetAlpha()
+		);
+
+		((Font *) sprite_mono->GetImage())->Render(text, sprite_mono->GetX(), sprite_mono->GetY());
+
+        // Refrescamos la pantalla 
+        screen.Refresh(); 
+    } 
+
+	rm.FreeResources();
+
+    return 0; 
+}
+
+/*#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
 
 #include "include/u-gine.h"
 #include <math.h>
@@ -44,7 +161,7 @@ int main(int argc, char* argv[])
     
 	while(screen.IsOpened() && !screen.KeyPressed(GLFW_KEY_ESC))
 	{ 
-        /* XINPUT STARTS */
+        // XINPUT STARTS 
         XINPUT_STATE state;
         memset(&state, 0, sizeof(state));
 
@@ -66,7 +183,7 @@ int main(int argc, char* argv[])
 
         float xPad = (float) state.Gamepad.sThumbLX / 32768;
         float yPad = (float) state.Gamepad.sThumbLY / 32768;
-        /* XINPUT ENDS */
+        // XINPUT ENDS 
 
 
 		int32 mouse_x = screen.GetMouseX();
@@ -143,3 +260,5 @@ int main(int argc, char* argv[])
 
     return 0; 
 }
+
+*/
